34e41c62c60a21bca039efe2c101a4f100b61286
malikumair319 <malikumairahmed0@gmail.com>
2024-01-05 10:54:12 +0500

    zabtech assignment

---
+++ assignment.dart
@@ -0,0 +1,28 @@
+
+
+void main(){
+
+  String name=('my name is: malik umair ');
+  print(name);  
+
+  String father=("my father name is: zubair ahmed");
+  print(father);
+
+  int age=23;
+  print('my age:  $age');
+
+  int num=03163112262;
+  print('phone number: $num');
+
+  String name1=('what is moye moye: kal menu ek kuri mili jiti galon vich toye ay moye moye');
+  print(name1);  
+
+  String line1=('moye moye moye moye moye moyeeeeeee');
+  print(line1);
+
+
+
+
+
+
+}
